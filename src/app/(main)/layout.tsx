5abe04bbc9b505533c770c3eeead7b2a41d6e40d
Rachit garg <rachitgarg56@gmail.com>
2025-03-12 12:59:13 +0530

    added font-classses needed and links in head, removed admin temporarily

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -26,8 +26,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200..800&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header/>
         {children}

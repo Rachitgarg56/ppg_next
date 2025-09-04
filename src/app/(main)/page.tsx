80da730748d7ef017afb1e7590d4fecb440051f7
Rachit garg <rachitgarg56@gmail.com>
2025-09-04 18:33:10 +0530

    added illustrations on homepage

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -39,8 +39,14 @@ export default function Home() {
 
       {/*  Buttons */}
       <div className="bg-black flex flex-col md:flex-row items-center justify-center space-x-4 pb-16">
-          <div className="w-80 h-80 rounded-full bg-[#178DA1] text-white flex items-center justify-center text-3xl font-semibold font-cambria italic">Work with us</div>
-          <div className="w-56 h-56 rounded-full bg-[#178DA1] mt-8 md:mt-48 text-white flex items-center justify-center text-2xl font-semibold font-cambria italic">Work for us</div>
+          <div className="relative w-80 h-80 rounded-full bg-[#178DA1] text-white flex items-center justify-center text-3xl font-semibold font-cambria italic">
+            <img className='w-full object-cover' src="/assets/images/home/work-with-us.png" alt="work-with-us" />
+            <span className='absolute top-[15%] cambria bold italic text-center'>Work <br /> with us</span>
+          </div>
+          <div className="relative w-56 h-56 rounded-full bg-[#178DA1] mt-8 md:mt-48 text-white flex items-center justify-center text-2xl font-semibold font-cambria italic">
+            <img className='w-full object-cover' src="/assets/images/home/work-for-us.png" alt="work-with-us" />
+            <span className='absolute top-[25%] left-[20%] cambria bold italic text-center'>Work <br /> for us</span>
+          </div>
       </div>
     </>
   );

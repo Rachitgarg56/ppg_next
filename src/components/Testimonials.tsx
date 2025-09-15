a27ba9edde5b4d5f411d93e5e1bc16464b4bc714
Rachit garg <rachitgarg56@gmail.com>
2025-09-15 11:36:59 +0530

    Update Testimonials.tsx

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -3,10 +3,9 @@
 import React, { useState } from 'react'
 
 const testimonials = [
-  {name: "Linda Smith", company: "Some Company 1", designation: "Some designation 1", text: "1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ad maxime tempora totam tempore incidunt distinctio doloribus cumque repellat molestias quos tenetur, quisquam, fugit corporis rem sequi. Molestias, sapiente quae.", },
-  {name: "Laura Potts", company: "Some Company 2", designation: "Some designation 2", text: "2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ad maxime tempora totam tempore incidunt distinctio doloribus cumque repellat molestias quos tenetur, quisquam, fugit corporis rem sequi. Molestias, sapiente quae.", },
-  {name: "James Camer", company: "Some Company 3", designation: "Some designation 3", text: "3. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ad maxime tempora totam tempore incidunt distinctio doloribus cumque repellat molestias quos tenetur, quisquam, fugit corporis rem sequi. Molestias, sapiente quae.", },
-  {name: "Franz Kafka", company: "Some Company 4", designation: "Some designation 4", text: "4. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ad maxime tempora totam tempore incidunt distinctio doloribus cumque repellat molestias quos tenetur, quisquam, fugit corporis rem sequi. Molestias, sapiente quae.", },
+  {name: "Brooke Bergeron", company: "Boston Beer Company", designation: "District Manager", text: "I’ve had the pleasure of working with Prestige Promotions for several years for events from small to large, and I can’t recommend them highly enough. Their team has proven to be incredibly reliable and trustworthy. They consistently show up on time, and always have a deep knowledge of our products. Prestige has become a valuable partner to Boston Beer Co.", },
+  {name: "Jeff Bee", company: "Constellation Brands", designation: "National Account Manager", text: "I have had the pleasure of working with Prestige Promotions for the past 10  plus years, and I cannot speak highly enough of their exceptional service and dedication. They have consistently gone above and beyond in conducting hand-selling events at Walmart locations for my business. Their team is professional, enthusiastic, and highly skilled at engaging customers and driving sales. I am incredibly grateful for the partnership we have built over the years. Prestige Promotions has been a key factor in our success, and I look forward to continuing our collaboration for many more years to come. If you are looking for a reliable and effective promotional partner, I highly recommend Prestige Promotions. Thank you, Prestige Promotions, for your outstanding work and unwavering commitment to excellence!", },
+  {name: "Tina Boyer", company: "SGWS", designation: "Account Executive", text: "Kourtney, You, as always sent an all-star for today's golf tournament. She just got the vibe and made sure everyone had fun. Five-star review. Thank you, Kourtney, for making my life easier when it comes to samplings and events. I know when I book events with your company, I can be assured a professional, prepared, representative will be there.", },
 ]
 
 const Testimonials = () => {
@@ -21,7 +20,7 @@ const Testimonials = () => {
   }
 
   return (
-    <div className="bg-[#178DA1] text-white text-center py-12 sm:py-24 md:py-48 overflow-hidden">
+    <div className="bg-[#178DA1] text-white text-center sm:py-12 overflow-hidden">
       <div className="relative w-full lg:w-2/3 mx-auto flex items-center justify-between p-4">
         
         {/* Prev button */}
@@ -38,8 +37,8 @@ const Testimonials = () => {
             style={{ transform: `translateX(-${index * 100}%)` }}
           >
             {testimonials.map((t, i) => (
-              <div key={i} className="w-full flex-shrink-0 px-4 sm:px-8 md:px-40 leading-relaxed">
-                <div className="text-xl md:text-2xl font-thin">{t.text}</div>
+              <div key={i} className="w-full flex-shrink-0 flex flex-col justify-center px-4 sm:px-8 md:px-40 leading-relaxed">
+                <div className="text-xl md:text-2xl font-thin">{i+1 + ". " + t.text}</div>
                 <div className="mt-8 md:mt-16 italic text-2xl md:text-3xl font-semibold">{t.name}</div>
                 <div className="mt-2 italic text-xl md:text-2xl font-thin">
                   {t.designation}, {t.company}

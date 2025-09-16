6fbfeb3a139215867e92ab7cbb07ff8d200ba63d
Rachit garg <rachitgarg56@gmail.com>
2025-09-16 11:00:22 +0530

    Update CoreValuesSection.tsx

--- src/components/CoreValuesSection.tsx
+++ src/components/CoreValuesSection.tsx
@@ -27,17 +27,18 @@ const CoreValuesSection = () => {
                     {
                         coreValues.map((value) => {
                             return (
-                                <div key={uuidv4()} className="w-full flex items-center mt-8 h-24 md:h-48 p-4">
-                                    <div className="h-24 md:h-48 w-24 md:w-48 rounded-full bg-gray-100"></div>
-                                    <div className="bg-gradient-to-r from-gray-700/10 via-gray-700 to-gray-700/10 shadow flex-1 p-4 md:p-16 h-24 md:h-48">
-                                        <div className="text-xl md:text-4xl italic text-white font-cambria">{value.title}</div>
-                                        <div className="text-xl font-semibold md:text-4xl text-[#178DA1]">{value.subtitle}</div>
+                                <div key={uuidv4()} className="w-full flex items-center mt-8 p-4">
+                                    {/* <div className="h-24 md:h-48 w-24 md:w-48 rounded-full bg-gray-100"></div> */}
+                                    <div className="bg-gradient-to-r from-gray-700/10 via-gray-700 to-gray-700/10 shadow p-4 md:p-16">
+                                        <div className="text-2xl md:text-4xl italic text-white font-cambria">{value.title}</div>
+                                        <div className="text-xl font-semibold md:text-3xl text-[#178DA1]">{value.subtitle}</div>
+                                        <div className='text-white text-lg md:text-2xl leading-relaxed font-light mt-2 md:mt-4'>{value.details}</div>
                                     </div>
-                                    <button className="text-4xl text-white" onClick={()=>showDetails(value)}>
+                                    {/* <button className="text-4xl text-white" onClick={()=>showDetails(value)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="size-9">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                                         </svg>
-                                    </button>
+                                    </button> */}
                                 </div>
                             )
                         })

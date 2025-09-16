fd1fbcdde6e868a87dbbde37abdd9a5c44656973
Rachit garg <rachitgarg56@gmail.com>
2025-09-16 10:34:48 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import { socials } from '@/utils/util';
 import Link from 'next/link';
@@ -20,6 +20,17 @@ const Header = () => {
 
     const paths = ['/careers', '/about-us', '/contact']
 
+    function handleKeyDown(e: KeyboardEvent) {
+        if (e.key == 'Escape') {
+            setShowMenu(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+        return () => window.removeEventListener('keydown', handleKeyDown);
+    }, [])
+
     return (
         <div className={`w-full p-8 max-sm:pb-0 md:pt-24 md:pb-16 md:px-24 ${paths.includes(pathname) ? 'bg-gradient-to-r from-[#125F6C] via-[#1C7A8A] via-30% to-[#178DA1]' : 'bg-black'} text-white ${showMenu && 'h-screen'}`}>
             {/* Header  */}
@@ -32,36 +43,51 @@ const Header = () => {
                 </button>
             </div>
             {/* Menu */}
-            <div className={`fixed overflow-hidden top-0 left-0 w-full h-screen bg-[#178DA1] p-10 sm:p-8 md:p-12 lg:px-40 lg:pt-32 z-20 ${!showMenu && 'hidden'}`}>
-                <button onClick={(() => setShowMenu(false))} className="cursor-pointer w-4 h-4 sm:w-12 md:w-24 sm:h-12 md:h-24 text-white absolute top-10 sm:top-8 md:top-8 lg:top-24 right-12 lg:right-40">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="size-6 sm:size-12 md:size-16 lg:size-20">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
-                </button>
-                <div className="flex flex-col space-y-4 sm:space-y-8 sm:ml-8  font-cambria">
-                    {
-                        menuItems.map((menuItem) => {
-                            return <Link onClick={()=>setShowMenu(false)} key={uuidv4()} href={menuItem.url} className="text-xl sm:text-3xl md:text-5xl italic font-semibold tracking-wide text-white hover:text-gray-100">{menuItem.text}</Link>
-                        })
-                    }
-                </div>
-                <div className="text-gray-100 flex flex-col md:flex-row items-start md:items-center justify-between sm:ml-8 mt-16 sm:mt-24">
-                    <div className="text-lg md:text-2xl">
-                        <div className="font-bold tracking-wide max-sm:text-sm">Prestige Promotions Group LLC</div>
-                        <div className="font-light tracking-wider max-sm:text-sm mt-2">St Louis, Missouri, United States</div>
-                    </div>
-                    <div className="flex mt-4 md:mt-0">
+            <div className={`fixed overflow-hidden flex flex-col justify-between top-0 left-0 w-full min-h-screen bg-gradient-to-r from-[#125F6C] via-[#1C7A8A] via-30% to-[#178DA1] z-20 ${!showMenu && 'hidden'}`}>
+                <div className='flex-3 sm:flex-3 flex items-center justify-center px-8'>
+                    <div className="relative flex flex-col space-y-8 font-cambria max-w-6xl w-full">
                         {
-                            socials.map((social) => {
-                                return (
-                                    <Link key={uuidv4()} href={social.url} target="_blank" className="w-8 h-8 sm:w-12 sm:h-12 drop-shadow-lg mr-4 md:ml-4">
-                                        <img src={`/assets/images/${social.icon}`} alt=""/>
-                                    </Link>        
-                                )
+                            menuItems.map((menuItem) => {
+                                return <Link onClick={()=>setShowMenu(false)} key={uuidv4()} href={menuItem.url} className="text-4xl sm:text-5xl italic font-semibold tracking-wide text-white hover:text-gray-100">{menuItem.text}</Link>
                             })
                         }
+                        <button onClick={(() => setShowMenu(false))} className="hidden cursor-pointer w-24 h-24 text-white absolute top-0 right-0 md:-top-4 md:-right-6 sm:flex items-center justify-center">
+                            <div className='w-full h-full relative'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="absolute top-0 right-0 size-12 md:size-16 lg:size-20">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                </svg>
+                            </div>
+                        </button>
                     </div>
-                </div>            
+                </div>
+
+                <div className="flex-1 bg-black text-gray-100 flex items-center justify-center py-8 md:py-16 px-8">
+                    <div className='flex items-center justify-between max-w-6xl w-full'>
+                        <div className="text-sm md:text-2xl">
+                            <div className="font-bold tracking-wide">Prestige Promotions Group LLC</div>
+                            <div className="font-light tracking-wider">St Louis, Missouri, United States</div>
+                        </div>
+                        <div className="flex space-x-2 md:space-x-4">
+                            {
+                                socials.map((social) => {
+                                    return (
+                                        <Link key={uuidv4()} href={social.url} target="_blank" className="w-8 md:w-12 h-8 md:h-12 drop-shadow-lg">
+                                            <img src={`/assets/images/${social.icon}`} alt=""/>
+                                        </Link>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>  
+
+                <button onClick={(() => setShowMenu(false))} className="cursor-pointer sm:hidden w-24 h-24 text-white absolute top-6 right-6 flex items-center justify-center">
+                    <div className='w-full h-full relative'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="absolute top-0 right-0 size-8 md:size-16 lg:size-20">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        </svg>
+                    </div>
+                </button>         
             </div>
         </div>
     )

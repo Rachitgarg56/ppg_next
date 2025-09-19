fdaf8164af081981d1baa9de86b137004f547cd8
Rachit garg <rachitgarg56@gmail.com>
2025-09-19 14:08:31 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -44,14 +44,14 @@ const Header = () => {
             </div>
             {/* Menu */}
             <div className={`fixed overflow-hidden flex flex-col justify-between top-0 left-0 w-full min-h-screen bg-gradient-to-r from-[#125F6C] via-[#1C7A8A] via-30% to-[#178DA1] z-20 ${!showMenu && 'hidden'}`}>
-                <div className='flex-3 sm:flex-3 flex items-center justify-center px-8'>
+                <div className='flex-1 flex items-center justify-center px-8 py-4'>
                     <div className="relative flex flex-col space-y-8 font-cambria max-w-6xl w-full">
                         {
                             menuItems.map((menuItem) => {
                                 return <Link onClick={()=>setShowMenu(false)} key={uuidv4()} href={menuItem.url} className="text-4xl sm:text-5xl italic font-semibold tracking-wide text-white hover:text-gray-100">{menuItem.text}</Link>
                             })
                         }
-                        <button onClick={(() => setShowMenu(false))} className="hidden cursor-pointer w-24 h-24 text-white absolute top-0 right-0 md:-top-4 md:-right-6 sm:flex items-center justify-center">
+                        <button onClick={(() => setShowMenu(false))} className="hidden cursor-pointer w-24 h-24 text-white absolute top-0 right-0 md:-top-4 md:-right-4 sm:flex items-center justify-center">
                             <div className='w-full h-full relative'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="absolute top-0 right-0 size-12 md:size-16 lg:size-20">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
@@ -60,9 +60,9 @@ const Header = () => {
                         </button>
                     </div>
                 </div>
-
-                <div className="flex-1 bg-black text-gray-100 flex items-center justify-center py-8 md:py-16 px-8">
-                    <div className='flex items-center justify-between max-w-6xl w-full'>
+                
+                <div className="bg-black text-gray-100 px-8 py-12 md:px-32 md:py-16">
+                    <div className='max-w-6xl w-full flex items-center justify-between mx-auto'>
                         <div className="text-sm md:text-2xl">
                             <div className="font-bold tracking-wide">Prestige Promotions Group LLC</div>
                             <div className="font-light tracking-wider">St Louis, Missouri, United States</div>
@@ -79,7 +79,7 @@ const Header = () => {
                             }
                         </div>
                     </div>
-                </div>  
+                </div> 
 
                 <button onClick={(() => setShowMenu(false))} className="cursor-pointer sm:hidden w-24 h-24 text-white absolute top-6 right-6 flex items-center justify-center">
                     <div className='w-full h-full relative'>
